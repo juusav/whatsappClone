97437c9b38cad7af709e9bc4925f24dbbb1d7915
Justice Velasco <justicevgomez001@gmail.com>
2021-10-07 23:56:48 +0200

    authentication created

--- App.tsx
+++ App.tsx
@@ -1,17 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { withAuthenticator } from 'aws-amplify-react-native';
+import Amplify from 'aws-amplify';
+import config from './src/aws-exports';
+import { Auth, API } from 'aws-amplify';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import Amplify from 'aws-amplify';
-import config from 'aws-amplify';
-Amplify.configure(config);
 
-export default function App() {
+Amplify.configure(config)
+
+function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  //Run this snippet only when  App is first mounted
+  useEffect(() => {
+    const fetchUSer = async () =>{
+      //Get authenticated user from Auth
+      const userInfo = await Auth.currentAuthenticatedUser({bypassCache: true});
+      
+      if(userInfo){
+        //Get the user from backend with user SUB from Auth
+        
+        //if there is no  user in DB with the id, then create one
+      }
+    }
+    fetchUSer();
+  }, [])
 
   if (!isLoadingComplete) {
     return null;
@@ -24,3 +42,5 @@ export default function App() {
     );
   }
 }
+
+export default withAuthenticator(App)
