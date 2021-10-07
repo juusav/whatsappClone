0457151ca3ea53df104986a3cf5de4d823e83af8
Justice Velasco <justicevgomez001@gmail.com>
2021-10-07 16:42:42 +0200

    amplify implemented

--- App.tsx
+++ App.tsx
@@ -5,6 +5,9 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import Amplify from 'aws-amplify';
+import config from 'aws-amplify';
+Amplify.configure(config);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
